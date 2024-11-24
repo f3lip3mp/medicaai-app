3deeeef62f8ee6ad44a922517788383b4ee523ee
f3lip3mp <pmenezesfelipe@gmail.com>
2024-11-24 08:09:07 -0300

    feat: dcria pÃagina de cadastro e direciona botao

--- app/src/main/java/com/example/medicaai_app/LoginActivity.java
+++ app/src/main/java/com/example/medicaai_app/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.medicaai_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.medicaai_app.databinding.ActivityLoginBinding;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        binding.textCadastro.setOnClickListener(v -> {
+            startActivity(new Intent(this, CadastroActivity.class));
         });
     }
 }
