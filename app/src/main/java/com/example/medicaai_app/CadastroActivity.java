322f3c86b03d46305742082a880662076b4c8a47
f3lip3mp <pmenezesfelipe@gmail.com>
2024-11-24 09:28:23 -0300

    feat: adiciona autenticacao com Firebase Auth

--- app/src/main/java/com/example/medicaai_app/CadastroActivity.java
+++ app/src/main/java/com/example/medicaai_app/CadastroActivity.java
@@ -1,24 +1,66 @@
 package com.example.medicaai_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CadastroActivity extends AppCompatActivity {
+import com.example.medicaai_app.databinding.ActivityCadastroBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class CadastroActivity extends AppCompatActivity {
+    private ActivityCadastroBinding binding;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        binding = ActivityCadastroBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_cadastro);
+        setContentView(binding.getRoot());
+        mAuth = FirebaseAuth.getInstance();
+        binding.btnCadastro.setOnClickListener(v -> validaDados());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+    }
+    private void validaDados(){
+        String email = binding.editEmail.getText().toString().trim();
+        String senha = binding.editSenha.getText().toString().trim();
+        if(!email.isEmpty()){
+            if(!senha.isEmpty()){
+                criarContaFirebase(email,senha);
+            }else{
+                Toast.makeText(this, "Informe uma senha.", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this, "Informe seu e-mail.", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void criarContaFirebase(String email, String senha){
+        mAuth.createUserWithEmailAndPassword(
+                email, senha
+        ).addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                startActivity(new Intent(this,  MainActivity.class));
+            }else{
+                Toast.makeText(this, "Ocorreu um erro.", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
