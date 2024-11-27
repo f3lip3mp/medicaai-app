1aa4b9f0d4d162bd8182f37a887034d8d4f705de
f3lip3mp <pmenezesfelipe@gmail.com>
2024-11-26 22:22:44 -0300

    feat: atribui redirecionamento para os botoes do menu bar

--- app/src/main/java/com/example/medicaai_app/InicioActivity.java
+++ app/src/main/java/com/example/medicaai_app/InicioActivity.java
@@ -7,18 +7,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.medicaai_app.databinding.ActivityInicioBinding;
+import com.example.medicaai_app.databinding.ActivityLoginBinding;
+import com.example.medicaai_app.databinding.ActivityRecuperaContaBinding;
 
 public class InicioActivity extends AppCompatActivity {
-
+    private ActivityInicioBinding binding;
+    private NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_inicio);
+        binding = ActivityInicioBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initNavigation();
     }
+
+    private void initNavigation() {
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();  // Usa a variável de instância
+        NavigationUI.setupWithNavController(binding.btnv, navController);
+    }
+
 }
