81b0e67c6529cdfcbc1355f80ada6d5103029ec0
f3lip3mp <pmenezesfelipe@gmail.com>
2024-11-29 18:36:40 -0300

    feat: cria exibicao de itens para medicamentos

---
+++ app/src/main/java/com/example/medicaai_app/adapter/MedicamentoAdapter.java
@@ -0,0 +1,4 @@
+package com.example.medicaai_app.adapter;
+
+public class MedicamentoAdapter {
+}
--- app/src/main/java/com/example/medicaai_app/model/Medicamento.java
+++ app/src/main/java/com/example/medicaai_app/model/Medicamento.java
@@ -1,4 +1,22 @@
 package com.example.medicaai_app.model;
 
 public class Medicamento {
+    String medicamentoNome;
+    String medicamentoDescricao;
+
+    public String getMedicamentoNome() {
+        return medicamentoNome;
+    }
+
+    public void setMedicamentoNome(String medicamentoNome) {
+        this.medicamentoNome = medicamentoNome;
+    }
+
+    public String getMedicamentoDescricao() {
+        return medicamentoDescricao;
+    }
+
+    public void setMedicamentoDescricao(String medicamentoDescricao) {
+        this.medicamentoDescricao = medicamentoDescricao;
+    }
 }
