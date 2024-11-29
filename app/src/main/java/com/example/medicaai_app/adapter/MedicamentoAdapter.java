72b7c9e45d01080a1ff4452753d01bfb684addc3
f3lip3mp <pmenezesfelipe@gmail.com>
2024-11-29 18:49:24 -0300

    feat: implementa adapter para referencias

--- app/src/main/java/com/example/medicaai_app/adapter/MedicamentoAdapter.java
+++ app/src/main/java/com/example/medicaai_app/adapter/MedicamentoAdapter.java
@@ -1,4 +1,32 @@
 package com.example.medicaai_app.adapter;
 
-public class MedicamentoAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MedicamentoAdapter extends RecyclerView.Adapter<MedicamentoAdapter.MedicamentoViewHolder> {
+    @NonNull
+    @Override
+    public MedicamentoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MedicamentoViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class MedicamentoViewHolder extends RecyclerView.ViewHolder{
+
+        public MedicamentoViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
